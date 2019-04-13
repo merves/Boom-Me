f0bcddda97054c1eaffcd3b142b1b37f0c1e715c
SENA3100 <snylmz147@gmail.com>
2019-04-13 16:28:28 +0300

    profil+orta

--- app/src/main/java/com/example/myapplication/ProfilActivity.java
+++ app/src/main/java/com/example/myapplication/ProfilActivity.java
@@ -1,13 +1,28 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ProfilActivity extends AppCompatActivity {
-
+    private Button btn_orta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        findViewById(R.id.orta_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ortaIntent= new Intent(getApplicationContext(),OrtaActivity.class);
+                startActivity(ortaIntent);
+                finish();
+            }
+        });
     }
+
+
 }
